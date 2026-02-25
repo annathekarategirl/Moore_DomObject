f97350069be2e83db4d32da03a12cff19bcabfa3
Anna Moore <annathekarategirl@gmail.com>
2026-02-25 14:09:03 -0500

    f

--- dom-homework.js
+++ dom-homework.js
@@ -2,3 +2,6 @@ const h1=document.createElement("h1")
 h1.textContent="Welcome to DOM homework"
 const myDiv=document.getElementById("myDiv")
 myDiv.append(h1)
+const p=document.createElement("p")
+p.textContent="This is your first DOM homework"
+myDiv.append(p)
