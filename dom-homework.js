63c2ea9252f409bbebe81310f670ebe51c2e80db
Anna Moore <annathekarategirl@gmail.com>
2026-02-26 12:47:49 -0500

    c

--- dom-homework.js
+++ dom-homework.js
@@ -5,3 +5,12 @@ myDiv.append(h1)
 const p=document.createElement("p")
 p.textContent="This is your first DOM homework"
 myDiv.append(p)
+const ul=document.createElement("ul")
+myDiv.append(ul)
+const l1=document.createElement("li")
+l1.textContent="one"
+const l2=document.createElement("li")
+l2.textContent="two"
+const l3=document.createElement("li")
+l3.textContent="three"
+ul.append(l1,l2,l3)
